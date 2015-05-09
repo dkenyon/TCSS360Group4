4368193f1350a9421c1522d9495fd1305be04bf7
dkenyon <dkenyon@uw.edu>
2015-05-09 16:52:10 -0700

    Update JobHandler.java
    - Added the addVolunteer method so that volunteers can be added into the master list.
    - Edited field and parameter names to meet group naming conventions
    - Added populate volunteers method to fill in list of volunteers from a given list
    - Added a populate jobs method to fill in list of jobs from a given list
    
    **Class still needs a Javadoc lookover

--- JobHandler.java
+++ JobHandler.java
@@ -1,31 +1,45 @@
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  * Represents an object which manages the list of available jobs for users to interact with
+ * 
+ * @author David Anderson, Dennis Kenyon, Brian Crabtree
+ * @version 09May2015
  */
+
 public class JobHandler {
 	
 	/** The list of jobs*/
-	private List<Job> jobList;
+	private List<Job> myJobList;
         
-        /** The list of volunteers*/
-        private List<Volunteer> volunteerList;
+    /** The list of volunteers*/
+    private List<Volunteer> myVolunteerList;
 	
 	/**
 	 * Initializing the JobHandler
 	 */
 	public JobHandler(){
-		jobList = new ArrayList<Job>();
-                volunteerList = new ArrayList<Volunteer>();
+		myJobList = new ArrayList<Job>();
+		myVolunteerList = new ArrayList<Volunteer>();
+	}
+	
+	/**
+	 * Adds the volunteer to the volunteer list.
+	 * @param theVolunteerToAdd the volunteer
+	 * @return true if success, false otherwise
+	 */
+	public boolean addVolunteer(Volunteer theVolunteerToAdd){
+		return myVolunteerList.add(theVolunteerToAdd);
 	}
 	
 	/**
 	 * Adds the given job object to the job list.
-	 * @param jobToAdd the job to add to the list
+	 * @param theJobToAdd the job to add to the list
 	 * @return true if success, false otherwise
 	 */
-	public boolean addJob(Job jobToAdd){
-		return jobList.add(jobToAdd);
+	public boolean addJob(Job theJobToAdd){
+		return myJobList.add(theJobToAdd);
 	}
 	
 	/**
@@ -33,58 +47,72 @@ public class JobHandler {
 	 * @param jobToDel the job to delete from the list
 	 * @return true if success, false otherwise
 	 */
-	public boolean deleteJob(Job jobToDel){
-		return jobList.remove(jobToDel);
+	public boolean deleteJob(Job theJobToDelete){
+		return myJobList.remove(theJobToDelete);
 	}
 	
-	/**
-	 * Finds a specified job matching the given unique jobID within the joblist and returns it
-	 * @param jobID the jobID to search for
-	 * @return job object matching coresponding jobID or null if job does not exit
-	 */
-	public Job getJobByID(int jobID){
-		Job ptr = null;
-		for(int index=0;index<jobList.size();index++){
-			ptr = jobList.get(index);
-			if(ptr.getJobID() == jobID){
-				return ptr;
-			}
-		}
-		return null;
-	}
+//	/**
+//	 * Finds a specified job matching the given unique jobID within the joblist and returns it
+//	 * @param theJobID the jobID to search for
+//	 * @return job object matching corresponding jobID or null if job does not exit
+//	 */
+//	public Job getJobByID(int theJobID){
+//		Job ptr = null;
+//		for(int index=0;index < myJobList.size();index++){
+//			ptr = myJobList.get(index);
+//			if(ptr.getJobID() == theJobID){
+//				return ptr;
+//			}
+//		}
+//		return null;
+//	}
 	
-	/**
-	 * Looks for jobs which the given volunteer has NOT signed up for
-	 * @param volunteerID in the volunteer to search for
-	 * @return a list of jobs which the volunteer has NOT signed up for
-	 */
-	public List<Job> getJobForVol(int volunteerID){
-		List<Job> returnList = new ArrayList<Job>();
-		Job ptr = null;
-		for(int index=0;index<jobList.size();index++){
-			ptr = jobList.get(index);
-			if(!ptr.containsVolunteer(volunteerID)){
-				returnList.add(ptr);
-			}
-		}
-		return returnList;
-	}
+//	/**
+//	 * Looks for jobs which the given volunteer has NOT signed up for
+//	 * @param theVolunteerID in the volunteer to search for
+//	 * @return a list of jobs which the volunteer has NOT signed up for
+//	 */
+//	public List<Job> getJobForVol(int theVolunteerID){
+//		List<Job> returnList = new ArrayList<Job>();
+//		Job ptr = null;
+//		for(int index=0;index < myJobList.size();index++){
+//			ptr = myJobList.get(index);
+//			if(!ptr.containsVolunteer(theVolunteerID)){
+//				returnList.add(ptr);
+//			}
+//		}
+//		return returnList;
+//	}
         
-        /**
-         * Looks for volunteers by last name
-         * @param lastName
-         * @return a list of volunteers with the specified last name
-         */
-        public List<Volunteer> getVolunteerByLastName(String lastName) {
+    /**
+     * Looks for volunteers by last name
+     * @param theLastName the last name of the volunteer to be searched for
+     * @return a list of volunteers with the specified last name
+     */
+    public List<Volunteer> getVolunteerByLastName(String theLastName) {
             
-            // list of volunteers with required name
-            List<Volunteer> matches = new ArrayList<>();
-            
-            // iterating for readability's sake
-            for(Volunteer aVolunteer : volunteerList) {
-                if(aVolunteer.getLastName().equalsIgnoreCase(lastName))
-                    matches.add(aVolunteer);
-            }
-            return matches;
+        // list of volunteers with required name
+        List<Volunteer> matches = new ArrayList<Volunteer>();
+        
+        // iterating for readability's sake
+        for(Volunteer aVolunteer : myVolunteerList) {
+            if(aVolunteer.getLastName().equalsIgnoreCase(theLastName))
+                matches.add(aVolunteer);
         }
+        return matches;
+    }
+    
+    public void printVolunteers() {
+    	for (Volunteer volunteer : myVolunteerList) {
+    		System.out.println(volunteer);
+    	}
+    }
+    
+    public void populateVolunteers(ArrayList<Volunteer> theList) {
+    	myVolunteerList = theList;
+    }
+    
+    public void populateJobs(ArrayList<Job> theList) {
+    	myJobList = theList;
+    }
 }
