86b1699bcc5373bf038ccd9155db165c498cfd9d
dash10 <dash@dashten.org>
2015-05-11 08:29:51 -0700

    fixed job test

--- JobTest.java
+++ JobTest.java
@@ -7,63 +7,72 @@ import org.junit.Test;
 
 public class JobTest {
 
-	private Job newJob;
-	private String jobName = "Test Job";
-	private int jobMonth = 2;
-	private int jobDate = 8;
-	private String jobLocation = "Woodland Park Zoo";
-	private int maxLight = 2;
-	private int maxMed = 3;
-	private int maxHeavy = 4;
-	private String theInfo = "This is where we post info about the job!";
+	private Job newJob1, newJob2, newJob3, newJob4, newJob5;
+        private Volunteer volunteer1, volunteer2, volunteer3, 
+                volunteer4, volunteer5;
+        private JobHandler myJobHandler = new JobHandler();
 	
 	@Before
 	public void setUp(){
-		newJob = new Job(jobName,jobMonth,jobDate,jobLocation,maxLight,maxMed,maxHeavy,theInfo);
+		newJob1 = new Job("Test Job 1", 2, 8, "Woodland Park Zoo", 2, 3, 4, "This job has lots of info.");
+		newJob2 = new Job("Test Job 2", 3, 9, "Woodland Park Zoo", 2, 3, 4, "This job has lots of info.");
+		newJob3 = new Job("Test Job 2", 4, 10, "Woodland Park Zoo", 2, 3, 4, "This job has lots of info.");
+		newJob4 = new Job("Test Job 2", 5, 11, "Woodland Park Zoo", 2, 3, 4, "This job has lots of info.");
+		newJob5 = new Job("Test Job 3", 6, 12, "Woodland Park Zoo", 2, 3, 4, "This job has lots of info.");
+                volunteer1 = new Volunteer("Abe1", "Lincoln1", "anonymous1@ftp.com", 
+                "123-456-7890", "123 Broad St. Nowhere, OK 73038", myJobHandler);
+                volunteer2 = new Volunteer("Abe2", "Lincoln2", "anonymous2@ftp.com", 
+                "123-456-7890", "123 Broad St. Nowhere, OK 73038", myJobHandler);
+                volunteer3 = new Volunteer("Abe3", "Lincoln3", "anonymous3@ftp.com", 
+                "123-456-7890", "123 Broad St. Nowhere, OK 73038", myJobHandler);
+                volunteer4 = new Volunteer("Abe4", "Lincoln4", "anonymous4@ftp.com", 
+                "123-456-7890", "123 Broad St. Nowhere, OK 73038", myJobHandler);
+                volunteer5 = new Volunteer("Abe5", "Lincoln5", "anonymous5@ftp.com", 
+                "123-456-7890", "123 Broad St. Nowhere, OK 73038", myJobHandler);
 	}
 	
 	@Test
 	public void testSignUpForLight() {
-		assertTrue(newJob.signUpForLight(1));
-		assertFalse(newJob.signUpForLight(1));
-		assertTrue(newJob.signUpForLight(2));
-		assertFalse(newJob.signUpForLight(3));
+		assertTrue(newJob1.signUpForLight(volunteer1));
+		assertFalse(newJob1.signUpForLight(volunteer1));
+		assertTrue(newJob1.signUpForLight(volunteer2));
+		assertFalse(newJob1.signUpForLight(volunteer2));
 	}
 
 	@Test
 	public void testSignUpForMedium() {
-		assertTrue(newJob.signUpForMedium(1));
-		assertFalse(newJob.signUpForMedium(1));
-		assertTrue(newJob.signUpForMedium(2));
-		assertTrue(newJob.signUpForMedium(4));
-		assertFalse(newJob.signUpForMedium(3));
+		assertTrue(newJob2.signUpForMedium(volunteer1));
+		assertFalse(newJob2.signUpForMedium(volunteer1));
+		assertTrue(newJob2.signUpForMedium(volunteer2));
+		assertTrue(newJob2.signUpForMedium(volunteer3));
+		assertFalse(newJob2.signUpForMedium(volunteer3));
 	}
 
 	@Test
 	public void testSignUpForHeavy() {
-		assertTrue(newJob.signUpForHeavy(1));
-		assertFalse(newJob.signUpForHeavy(1));
-		assertTrue(newJob.signUpForHeavy(2));
-		assertTrue(newJob.signUpForHeavy(3));
-		assertTrue(newJob.signUpForHeavy(4));
-		assertFalse(newJob.signUpForHeavy(5));
+		assertTrue(newJob3.signUpForHeavy(volunteer1));
+		assertFalse(newJob3.signUpForHeavy(volunteer1));
+		assertTrue(newJob3.signUpForHeavy(volunteer2));
+		assertTrue(newJob3.signUpForHeavy(volunteer3));
+		assertTrue(newJob3.signUpForHeavy(volunteer4));
+		assertFalse(newJob3.signUpForHeavy(volunteer5));
 	}
 
 	@Test
 	public void testContainsVolunteer() {
-		assertFalse(newJob.containsVolunteer(1));
-		newJob.signUpForLight(3);
-		assertTrue(newJob.containsVolunteer(3));
+		assertFalse(newJob1.containsVolunteer(volunteer5));
+		newJob4.signUpForLight(volunteer5);
+		assertTrue(newJob1.containsVolunteer(volunteer5));
 	}
 
 	@Test
 	public void testGetVolunteers() {
-		List<Integer> test = newJob.getVolunteers();
+		List<Volunteer> test = newJob1.getVolunteers();
 		assertEquals(0,test.size());
-		newJob.signUpForLight(1);
-		newJob.signUpForMedium(2);
-		newJob.signUpForHeavy(4);
-		test = newJob.getVolunteers();
+		newJob5.signUpForLight(volunteer1);
+		newJob5.signUpForMedium(volunteer2);
+		newJob5.signUpForHeavy(volunteer3);
+		test = newJob1.getVolunteers();
 		assertEquals(3,test.size());
 	}
 
