8c1b9fb9618fe0020cf77c9842fe593e5624f8f6
djander <djander@uw.edu>
2015-05-31 14:40:11 -0700

    Updated tests
    Removed tests which were not being handled by volunteer class (such as checking if a job was full). Moved tests into their own methods.

--- VolunteerTest.java
+++ VolunteerTest.java
@@ -4,16 +4,20 @@
  */
 
 import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class VolunteerTest {
     
     private Volunteer myVolunteer1, myVolunteer2;
-    private Job myJob1, myJob2, myJob3, myJob4, myJob5, myJob6;
+    private Job myJob1, myJob2, myJob3, myJob4;
     private static JobHandler myJobHandler = new JobHandler();
+    private ArrayList<Job> myJobList = new ArrayList<Job>();
     
     @Before
     public void setUp() {
@@ -32,43 +36,17 @@ public class VolunteerTest {
                 "This job involves redundant tasks.");
         myJob3 = new Job("aJobName3", curMonth, curDate - 1, "park 3", 1, 1, 1, 
                 "This job involves repulsive tasks.");
-        myJob4 = new Job("aJobName4", curMonth + 1, curDate + 1, "Park 4", 1, 1, 1, 
+        myJob4 = new Job("aJobName4", curMonth + 1, curDate, "Park 4", 1, 1, 1, 
                 "This job involves reordered tasks.");
-        myJob5 = new Job("aJobName5", curMonth + 1, curDate + 2, "Park 4", 1, 1, 1, 
-                "This job involves reconditioned tasks.");
-        myJob6 = new Job("aJobName6", curMonth + 2, curDate + 3, "Park 4", 1, 1, 1, 
-                "This job involves recycled tasks.");
         
-        myJobHandler.addJob(myJob1);
-        myJobHandler.addJob(myJob2);
-        myJobHandler.addJob(myJob3);
-        myJobHandler.addJob(myJob4);
-        myJobHandler.addJob(myJob5);
-        myJobHandler.addJob(myJob6);
+        myJobList.add(myJob1);
+        myJobList.add(myJob2);
+        myJobList.add(myJob3);
+        myJobList.add(myJob4);
+        
+        myJobHandler.populateJobs(myJobList);
 
-    }
-    
-    /**
-     * Test method for {@link Volunteer#toString()}
-     */
-    @Test
-    public void testToString() {
-        assertEquals("toString() produced an unexpected result!",
-                     "Name: Abe Lincoln    UserID: 00001", myVolunteer1.toString());
-    }
-    
-    /**
-     * Test method for {@link getUserID()}
-     */
-    @Test
-    public void testGetUserID() {
-        
-        assertEquals("getUserID() returned unexpected value",
-                1, myVolunteer1.getUserID());
-        assertEquals("getUserID() returned unexpected value",
-                2, myVolunteer2.getUserID());
-        assertTrue("userID failed to increment", 
-                myVolunteer1.getUserID() < myVolunteer2.getUserID());
+
     }
     
     /**
@@ -88,19 +66,24 @@ public class VolunteerTest {
     public void testSignUpForJob() {
         assertTrue("unable to signUpForJob()",
                 myVolunteer1.signUpForJob(myJob1, 1));
-        assertTrue("unable to sign up 2 people for the same job",
-                myVolunteer2.signUpForJob(myJob1, 2));
+    }
+    
+    @Test
+    public void testSignUpForJobinPast(){
         assertFalse("was able to sign up for job from last month",
                 myVolunteer1.signUpForJob(myJob2, 1));
-        assertFalse("was able to sign up for job from last day",
-                myVolunteer1.signUpForJob(myJob3, 1));
-        assertFalse("was able to sign up for second job on same day",
-                myVolunteer1.signUpForJob(myJob4, 1));
-        assertTrue("unable to sign up for job",
-                myVolunteer1.signUpForJob(myJob5, 1));
-        assertFalse("exceeded medium volunteer limit, still able to volunteer",
-                myVolunteer2.signUpForJob(myJob5, 1));
-        
+    }
+    
+    @Test
+    public void testSignUpForJobYesterday(){
+    	assertFalse("was able to sign up for job that took place yesterday",
+    			myVolunteer1.signUpForJob(myJob3, 1));
+    }
+    
+    @Test
+    public void testSignUpForJobToday(){
+    	assertFalse("was able to sign up for job that took place today",
+    			myVolunteer1.signUpForJob(myJob4, 1));    	
     }
     
     /**
@@ -108,7 +91,7 @@ public class VolunteerTest {
      */
     @Test
     public void testViewJobsSignedUpFor() {
-        myVolunteer1.signUpForJob(myJob6, 1);
+        myVolunteer1.signUpForJob(myJob1, 1);
         assertTrue("viewJobsSignedUpFor() returned unexpected result",
                 myVolunteer1.getJobs().contains(myJob1));
         assertFalse("viewJobsSignedUpFor() returned job not signed up for",
